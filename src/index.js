c4af72e669f9bc39795e44c08e189f0d5155e8e6
1e1 <1e1@users.noreply.github.com>
2018-07-31 23:16:50 +0200

    make oem SD_CARD
    get the original repo of MyPOIs

--- src/index.js
+++ src/index.js
@@ -3,9 +3,16 @@
     const COUNTRIES = [ 'FR' ];
     const FORMATS = [ 'csv', 'ov2', 'gpx' ];
 
+    let formats = process.argv.slice(2);
+    if (0 === formats.length) {
+        formats = FORMATS;
+    } else {
+        formats = formats.filter(format => FORMATS.includes(format));
+    }
+
     COUNTRIES.forEach(async country => {
         const launcher = require('./modules/' + country + '.js');
-        const crawler = launcher.from(FORMATS);
+        const crawler = launcher.from(formats);
         
         console.log(crawler.getCode() + ' is running');
 
