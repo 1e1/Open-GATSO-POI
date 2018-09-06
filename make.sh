3531312c3448008550efc4ce34b5152ff683dbe6
1e1 <1e1@users.noreply.github.com>
2018-09-06 18:38:55 +0200

    refactor

---
+++ make.sh
@@ -0,0 +1,143 @@
+#!/bin/bash
+
+
+BASE_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )
+BUILD_PATH="$BASE_DIR/BUILD"
+RELEASE_PATH="$BASE_DIR/RELEASES"
+MOUNT_PATH="$BASE_DIR/SD_CARD"
+MANIFEST_PATH="$BUILD_PATH/manifest.txt"
+VERSIONS_PATH="$BUILD_PATH/versions.txt"
+
+
+make_zip()
+{
+    if [ -d $BUILD_PATH ]
+    then
+      EXT=$1
+
+      mkdir "${BUILD_PATH}_${EXT}"
+      cp -R ${BUILD_PATH}/*.{bmp,$EXT} "${BUILD_PATH}_${EXT}/"
+      zip -r "${RELEASE_PATH}/${EXT}_files.zip" "${BUILD_PATH}_${EXT}/"
+      rm -rf "${BUILD_PATH}_${EXT}"
+    fi
+}
+
+
+_init()
+{
+    [ ! -d $BUILD_PATH ] && mkdir $BUILD_PATH
+    [ ! -d $RELEASES   ] && mkdir $RELEASES
+}
+
+
+_install()
+{
+    bash $BASE_DIR/mypois_ctl.sh --install
+}
+
+
+_clean()
+{
+    [ -d $BUILD_PATH ] && rm -rf $BUILD_PATH
+
+    bash $BASE_DIR/mypois_ctl.sh --clean
+}
+
+
+_build()
+{
+    node $BASE_DIR/src/build.js
+}
+
+
+_release()
+{
+    [ -d $BUILD_PATH   ] && zip -r $RELEASE_PATH/all_files.zip $BUILD_PATH
+    make_zip csv
+    make_zip gpx
+    make_zip ov2
+    [ -d $MOUNT_PATH   ] && zip -r $RELEASE_PATH/sd_files.zip $MOUNT_PATH
+}
+
+
+_unrelease()
+{
+    [ -d $RELEASES   ] && rm -rf $RELEASES
+}
+
+
+_mount()
+{
+    bash $BASE_DIR/mypois_ctl.sh --make
+}
+
+
+_image()
+{
+    CMD='genisoimage'
+
+    if [ ! `command -v $CMD` ]
+    then
+        CMD='mkisofs'
+    fi
+
+    genisoimage -o $BUILD_PATH/sd_image.iso $MOUNT_PATH
+    zip -r $RELEASE_PATH/sd_image.iso.zip $BUILD_PATH/sd_image.iso
+    rm -f $BUILD_PATH/sd_image.iso
+}
+
+
+_update_doc()
+{
+    bash $BASE_DIR/mypois_ctl.sh --update-version
+    node $BASE_DIR/src/update_doc.js
+}
+
+
+for opt in "$@"
+do
+  case $opt in
+  "--init")
+    _init
+    ;;
+  "-i"|"--install")
+    _init
+    _install
+    ;;
+  "-c"|"--clean")
+    _clean
+    ;;
+  "--unrelease")
+    _unrelease
+    ;;
+  "-b"|"--build")
+    _build
+    ;;
+  "-z"|"--release")
+    _release
+    ;;
+  "-sd"|"--mount")
+    _mount
+    _update_doc
+    ;;
+  "-iso"|"--image")
+    _image
+    ;;
+  "-doc"|"--update-doc")
+    _update_doc
+    ;;
+  "--standalone")
+    _init
+    _build
+    _unrelease
+    _release
+    _mount
+    _update_doc
+    ;;
+  *|"--make")
+    _build
+    _release
+    _mount
+    _update_doc
+  esac
+done
