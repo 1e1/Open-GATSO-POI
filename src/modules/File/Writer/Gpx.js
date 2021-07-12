c19088d149a3f805b19c60d3a8f575d231b7d61b
1e1 <2.71828183e0+git@gmail.com>
2021-07-12 23:19:40 +0200

    fix #128

--- src/modules/File/Writer/Gpx.js
+++ src/modules/File/Writer/Gpx.js
@@ -4,7 +4,7 @@ module.exports = class Gpx extends WRITER {
 
     setHeader() {
         const header = '<?xml version="1.0" encoding="UTF-8" standalone="no" ?>' + "\n"
-            + '<gpx xmlns="http://www.topografix.com/GPX/1/1" xmlns:gpxx="http://www.garmin.com/xmlschemas/GpxExtensions/v3" xmlns:gpxtpx="http://www.garmin.com/xmlschemas/TrackPointExtension/v1" creator="Oregon 400t" version="1.1" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd http://www.garmin.com/xmlschemas/GpxExtensions/v3 http://www.garmin.com/xmlschemas/GpxExtensionsv3.xsd http://www.garmin.com/xmlschemas/TrackPointExtension/v1 http://www.garmin.com/xmlschemas/TrackPointExtensionv1.xsd">'
+            + '<gpx version="1.1" xmlns="http://www.topografix.com/GPX/1/1" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd">'
             ;
         
         this.header = Buffer.from(header, this.fileEncoding);
